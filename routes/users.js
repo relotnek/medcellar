8a74bb5283c4e8d53a48b241448a947a2eb2de72
relotnek <kentoler@gmail.com>
2015-03-30 12:20:21 -0400

    added users js for auth

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,16 @@
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 SALT_WORK_FACTOR = 10;
 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('meddb', server, {safe: true});
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'meddb' database");
-        db.collection('users', {safe:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'users' collection doesn't exist. Creating it with sample data...");
-                populateDB();
-            }
-        });
-    }
+mongoose.connect('localhost','meddb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log('Connected to Users');
 });
-
 
 var userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -41,74 +30,37 @@ userSchema.pre('save', function(next){
     });
 });
 
-
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving med: ' + id);
-    db.collection('users', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, user) {
-            res.send(user);
-        });
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
     });
 };
 
-exports.findAll = function(req, res) {
-    db.collection('users', function(err, collection) {
-        collection.find().toArray(function(err, users) {
-            res.send(users);
-        });
-    });
-};
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-exports.addUser = function(req, res) {
-    var med = req.body;
-    console.log('Adding med: ' + JSON.stringify(med));
-    db.collection('meds', function(err, collection) {
-        collection.insert(med, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
-}
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
-exports.updateUser = function(req, res) {
-    var id = req.params.id;
-    var med = req.body;
-    delete med._id;
-    console.log('Updating med: ' + id);
-    console.log(JSON.stringify(med));
-    db.collection('meds', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, med, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating med: ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.send(med);
-            }
-        });
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.findOne({ username: username }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) return done(err);
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Invalid password' });
+      }
     });
-}
-
-exports.deleteUser = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting med: ' + id);
-    db.collection('meds', function(err, collection) {
-        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
-    });
-}
-
+  });
+}));
 /*--------------------------------------------------------------------------------------------------------------------*/
 // Populate database with sample data -- Only used once: the first time the application is started.
 // You'd typically not find this code in a real-life app, since the database would already exist.
