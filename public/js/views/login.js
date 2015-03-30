08ba2d7dab255c36437e22e623bf7467c925a017
relotnek <kentoler@gmail.com>
2015-03-30 16:13:58 -0400

    cleaned loginview.html

--- public/js/views/login.js
+++ public/js/views/login.js
@@ -10,32 +10,11 @@ window.LoginView = Backbone.View.extend({
     },
 
     events: {
-        "change"        : "change",
-        "click .save"   : "beforeSave",
-        "click .delete" : "deleteMed",
-        "drop #picture" : "dropHandler"
+        "click .login"   : "goLogin",
+        "click .cancel" : "goHome",
     },
 
-    change: function (event) {
-        // Remove any existing alert message
-        utils.hideAlert();
-
-        // Apply the change to the model
-        var target = event.target;
-        var change = {};
-        change[target.name] = target.value;
-        this.model.set(change);
-
-        // Run validation rule (if any) on changed item
-        var check = this.model.validateItem(target.id);
-        if (check.isValid === false) {
-            utils.addValidationError(target.id, check.message);
-        } else {
-            utils.removeValidationError(target.id);
-        }
-    },
-
-    beforeSave: function () {
+    goLogin: function () {
         var self = this;
         var check = this.model.validateAll();
         if (check.isValid === false) {
@@ -46,44 +25,17 @@ window.LoginView = Backbone.View.extend({
         return false;
     },
 
-    saveMed: function () {
+    goHome: function () {
         var self = this;
         console.log('before save');
         this.model.save(null, {
             success: function (model) {
-                self.render();
-                app.navigate('meds/' + model.id, false);
-                utils.showAlert('Success!', 'Med saved successfully', 'alert-success');
+                app.navigate('meds', false);
             },
             error: function () {
                 utils.showAlert('Error', 'An error occurred while trying to delete this item', 'alert-error');
             }
         });
-    },
-
-    deleteMed: function () {
-        this.model.destroy({
-            success: function () {
-                alert('Med deleted successfully');
-                window.history.back();
-            }
-        });
-        return false;
-    },
-
-    dropHandler: function (event) {
-        event.stopPropagation();
-        event.preventDefault();
-        var e = event.originalEvent;
-        e.dataTransfer.dropEffect = 'copy';
-        this.pictureFile = e.dataTransfer.files[0];
-
-        // Read the image file from the local file system and display it in the img tag
-        var reader = new FileReader();
-        reader.onloadend = function () {
-            $('#picture').attr('src', reader.result);
-        };
-        reader.readAsDataURL(this.pictureFile);
     }
 
 });
