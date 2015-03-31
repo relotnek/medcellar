92646885c12aa0b4e0eb18ed187e0962989b4631
relotnek <kentoler@gmail.com>
2015-03-31 16:16:16 -0400

    made grunt create and drop

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,48 @@
+
 var db = require('./grunt/db/db');
+var _ = require('underscore');
+
+
+var users = [
+	{
+		username: 'ktoler',
+		email: 'ken@medcellar.com',
+		password: 'slapdabass'
+	},
+	{
+		username: 'bob',
+		email: 'bob@medcellar.com',
+		password: 'bobdole'
+	},
+	{
+		username: 'gellerb',
+		email: 'geller@medcellar.com',
+		password: 'magellan'
+	},
+];
 
 module.exports = function(grunt) {
-	grunt.registerTask('builddb', 'populate the database', function() {
-	//BUILD DB
-	done = this.async();
-	var user = new db.user({ username: 'ktoler', email: 'ktoler@ktoler.com', password: 'slapdabass'});
-	console.log(user);
-    user.save(function(err){
-    	if(err){
-    		console.log(err);
-    	} else {
-    		console.log("User: " + user.username + " Saved.");
-    	}
-    	db.db.close(done);
-
-    });
+	grunt.initConfig({
+	  exec: {
+	    dropdb: 'mongo meddb --eval "db.dropDatabase()"'
+	  }
 	});
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.registerTask('builddb', 'populate the database', function() {
+		//BUILD DB
+		var done = this.async();
+		_.each(users, function(insertion){
+		var user = new db.user({ username: insertion.username, email: insertion.email , password: insertion.password });
+		console.log(user);
+		user.save(function(err){
+			if(err){
+				console.log(err);
+			} else {
+				console.log("User: " + user.username + " Saved.");
+			}
+			done();
+		});
+		});
+	});
+
 };
