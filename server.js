aabc2c6a26f717661a9c5dd2bf3fa973d794621d
relotnek <kentoler@gmail.com>
2015-03-31 11:30:02 -0400

    comments

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.logger('dev'));
+    // Express config
     app.use(express.bodyParser());
     app.use(express.cookieParser());
     app.use(express.methodOverride());
     app.use(express.session({secret: 'applesandpears'}));
+    // Passport config
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(express.static(path.join(__dirname,'public')));
