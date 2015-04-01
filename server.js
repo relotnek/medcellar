3f62f486996acc5dcb8f5dfe76270cf01f120711
Geller <geller@Gellers-MacBook-Pro.local>
2015-04-01 10:51:23 -0400

    Code style changes applied to server.js

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    passport = require('passport');
-    med = require('./routes/meds');
+    passport = require('passport'),
+    med = require('./routes/meds'),
     user = require('./routes/users');
 
 var app = express();
 
-app.configure(function () {
+
+app.configure(function() {
     app.set('port', process.env.PORT || 3000);
     app.use(express.logger('dev'));
-    // Express config
     app.use(express.bodyParser());
     app.use(express.cookieParser());
     app.use(express.methodOverride());
-    app.use(express.session({secret: 'applesandpears'}));
-    // Passport config
+    app.use(express.session({
+        secret: 'applesandpears'
+    }));
     app.use(passport.initialize());
     app.use(passport.session());
-    app.use(express.static(path.join(__dirname,'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.post('/login', user.login );
+
+app.post('/login', user.login);
 app.get('/meds', user.ensureAuth, med.findAll);
 app.get('/meds/:id', user.ensureAuth, med.findById);
 app.post('/meds', user.ensureAuth, med.addMed);
 app.put('/meds/:id', user.ensureAuth, med.updateMed);
 app.delete('/meds/:id', user.ensureAuth, med.deleteMed);
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), function() {
     console.log("Express server listening on port " + app.get('port'));
 });
