b14415c0b3555bbca662699f03a18ee17e93ed8c
Geller <geller@Gellers-MacBook-Pro.local>
2015-04-02 11:31:04 -0400

    Updated users.js and db.js with bcrypt & md5

--- grunt/db/db.js
+++ grunt/db/db.js
@@ -4,9 +4,9 @@ var crypto = require('crypto-js');
 var bcrypt = require('bcrypt');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var SALT_WORK_FACTOR = 10;
+var SALT_WORK_FACTOR = 1;
 
-mongoose.connect('localhost','meddb');
+mongoose.connect('localhost', 'meddb');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
@@ -14,10 +14,30 @@ db.once('open', function callback() {
 });
 
 var userSchema = mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true},
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    hashDigest: {
+        type: String,
+        required: true
+    },
+    workFactor: {
+        type: Number,
+        required: false
+    }
 });
+
 
 var medSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true},
@@ -32,32 +52,33 @@ var medSchema = mongoose.Schema({
   picture: { type: String, required: false, unique: false}
 });
 
-userSchema.pre('save', function(next){
-// TODO - Add ability to switch betwen MD5 & BCrypt for password storage
+userSchema.pre('save', function(next) {
+    // TODO - Add ability to switch betwen MD5 & BCrypt for password storage
 
-  var user = this;
+    var user = this;
 
-	if(!user.isModified('password')) return next();
+    if (user.hashDigest.toLowerCase() === 'md5') {
+        try {
+            user.password = crypto.MD5(user.password).toString();
+            next();
+        } catch (ex) {
+            next(ex);
+        }
+    }
 
-// usage of md5 for password storage
-// TODO - Add MD5 storage
-// TODO - Add Error Handling
-//user.password = crypto.MD5(user.password).toString();
-//next();
-// catch (ex)
-// next(ex);
+    if (user.hashDigest.toLowerCase() === 'bcrypt') {
+        bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+            if (err) return next(err);
 
-// usage of bcrypt for password storage
-bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if(err) return next(err);
-
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if(err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    }
 });
+
 
 var Med = mongoose.model('Med', medSchema);
 var User = mongoose.model('User', userSchema);
